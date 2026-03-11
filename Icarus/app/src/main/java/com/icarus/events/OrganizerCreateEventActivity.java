58488e35d78a0f08c248fa1876fb4a6e9eb9c3b3
Waresalmon <86568693+Waresalmon@users.noreply.github.com>
2026-03-10 20:55:02 -0600

    Created activity_create_event xml that will be used for organizers to create events. Functionality is first focus.

---
+++ Icarus/app/src/main/java/com/icarus/events/OrganizerCreateEventActivity.java
@@ -0,0 +1,14 @@
+package com.icarus.events;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OrganizerCreateEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+    }
+}
