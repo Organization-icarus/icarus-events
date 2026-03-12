fc995e023eb1db713d17b47712e7355c8ece4333
Kito <kleeson@ualberta.ca>
2026-03-11 19:15:24 -0600

    adding javadoc to EntrantEventListArrayAdapter

--- Icarus/app/src/main/java/com/icarus/events/EntrantEventListArrayAdapter.java
+++ Icarus/app/src/main/java/com/icarus/events/EntrantEventListArrayAdapter.java
@@ -14,15 +14,40 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Locale;
 
+/**
+ * Adapter used to display events in the entrant event list.
+ * <p>
+ * Binds Event objects to the entrant event list item layout and displays
+ * basic event information such as name, category, and formatted event date.
+ *
+ * @author Alex Alves
+ */
 public class EntrantEventListArrayAdapter extends ArrayAdapter<Event> {
     private ArrayList<Event> events;
     private Context context;
+
+    /**
+     * Constructs an adapter for displaying Event objects in the entrant event list.
+     *
+     * @param context the context used to inflate views and access resources
+     * @param events the list of events to be displayed by the adapter
+     */
     public EntrantEventListArrayAdapter(Context context, ArrayList<Event> events) {
         super(context, 0, events);
         this.events = events;
         this.context = context;
     }
 
+    /**
+     * Returns a view for displaying an event in the entrant event list.
+     * Inflates the list item layout if necessary and binds the event's
+     * name, category, and formatted date to the corresponding UI elements.
+     *
+     * @param position the position of the event in the adapter's data set
+     * @param convertView a recycled view to reuse if available
+     * @param parent the parent view that this view will be attached to
+     * @return the view representing the event at the specified position
+     */
     @NonNull
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent){
         View view = convertView;
