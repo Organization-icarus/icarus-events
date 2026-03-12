057f14caee5466d36ac47852044beb42c02bc0d4
Kito <kleeson@ualberta.ca>
2026-03-11 21:41:15 -0600

    adding javadoc to EventHistoryArrayAdapter

--- Icarus/app/src/main/java/com/icarus/events/EventHistoryListArrayAdapter.java
+++ Icarus/app/src/main/java/com/icarus/events/EventHistoryListArrayAdapter.java
@@ -14,15 +14,40 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Locale;
 
+/**
+ * Adapter used to display a user's event history in a ListView.
+ * <p>
+ * Binds Event objects to the event history list item layout and displays
+ * basic event information such as the event name, category, and formatted date.
+ *
+ * @author Benjamin Hall
+ */
 public class EventHistoryListArrayAdapter extends ArrayAdapter<Event> {
     private ArrayList<Event> events;
     private Context context;
+
+    /**
+     * Constructs an adapter for displaying Event objects in the event history list.
+     *
+     * @param context the context used to inflate views and access resources
+     * @param events the list of events to be displayed by the adapter
+     */
     public EventHistoryListArrayAdapter(Context context, ArrayList<Event> events) {
         super(context, 0, events);
         this.events = events;
         this.context = context;
     }
 
+    /**
+     * Returns a view for displaying an event in the event history list.
+     * Inflates the list item layout if necessary and binds the event's
+     * name, category, and formatted date to the corresponding UI elements.
+     *
+     * @param position the position of the event in the adapter's data set
+     * @param convertView a recycled view to reuse if available
+     * @param parent the parent view that this view will be attached to
+     * @return the view representing the event at the specified position
+     */
     @NonNull
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent){
         View view = convertView;
